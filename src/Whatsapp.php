3c23f951305b9dfe4e2a1fc17300cadaf71bfa6e
Muhammad Arifin Habibi <arifinhabibi76@gmail.com>
2023-08-24 09:56:02 +0700

    merge tag

--- src/Whatsapp.php
+++ src/Whatsapp.php
@@ -6,8 +6,21 @@ class Whatsapp {
 
     function send(string $targetNumber, string $message)
     {
-        $convertMessage = explode(" ", $message);
-        $readySend = implode("+", $convertMessage);
-        return redirect("https://api.whatsapp.com/send/?phone=%2B62" . $targetNumber . "&text=". $readySend ."&text&type=phone_number&app_absent=0");
+        // filtering number
+        $convertingNumber = str_split($targetNumber);
+        $filterNumber = array_map(function($value){
+            if ($value == "0") {
+                return "62";
+            }
+            if ($value == "+") {
+                # code...
+                return "";
+            }
+        }, $convertingNumber);
+        $readyNumber = implode("", $filterNumber);
+
+        $convertingMessage = explode(" ", $message);
+        $readyMessage = implode("+", $convertingMessage);
+        return redirect("https://api.whatsapp.com/send/?phone=%2B" . $readyNumber . "&text=". $readyMessage ."&text&type=phone_number&app_absent=0");
     }
 }
